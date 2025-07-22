3683deb7303316076d30bcd7f4eb0fbe7c2dde2e
amit <singhajju250@gmail.com>
2025-07-22 22:11:49 +0530

    question 1 done

---
+++ ques_3.py
@@ -0,0 +1,12 @@
+# Declare an integer, a float, a string, and a boolean variable in Python.
+int_num = 34
+print(type(int_num))
+
+float_num = 13.4
+print(type(float_num))
+
+str = "to is hot"
+print(type(str))
+
+my_bool = True # boolean value are case-sensitive
+print(type(my_bool))
