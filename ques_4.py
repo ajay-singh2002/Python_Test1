189fb8425ebed2b60f139fffddbc44bb106bd4a4
amit <singhajju250@gmail.com>
2025-07-22 22:30:13 +0530

    question 1 done

---
+++ ques_4.py
@@ -0,0 +1,8 @@
+# Write a Python script to convert a string number to int and float.
+number = "25"
+number = int(number)
+print(type(number))
+
+number = "25"
+number = float(number)
+print(type(number))
