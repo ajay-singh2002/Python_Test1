2dbbb9304e66c9aa98a283d71f52fe908058c27c
amit <singhajju250@gmail.com>
2025-07-22 23:02:20 +0530

    question 1 done

---
+++ ques_7.py
@@ -0,0 +1,24 @@
+# Perform all arithmetic operations (+, -, *, /, //, %) on two integers
+a = 20
+b = 10
+print(a+b)
+
+a = 20
+b = 10
+print(a-b)
+
+a = 20
+b = 10
+print(a*b)
+
+a = 20
+b = 10
+print(a/b)
+
+a = 20
+b = 10
+print(a//b)
+
+a = 20
+b = 10
+print(a%b)
