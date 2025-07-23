4d57e44936d02a0b724536bef5a0df8d9f9cf142
amit <singhajju250@gmail.com>
2025-07-23 23:28:16 +0530

    question 1 done

---
+++ ques_15.py
@@ -0,0 +1,6 @@
+# Write a while loop that prints numbers from 10 to 1.
+num = 10            # starting number
+
+while num >= 1:     # loop chalega jab tak num 1 ya usse bada hai
+    print(num)
+    num -= 1        # har baar num ko 1 se kam kar do
